5d9ed4de240cb63b564966a3cde9d3f7512ae5c6
Sbonokuhle <b.sbonokuhle@gmail.com>
2023-02-20 06:06:10 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry
- * return: Always (0)
+ * main - Print a line of code using puts
+ * Return: 0
  */
 int main(void)
-{
-	 puts("\"Programming is like building a multi lingual puzzle");
-	 retutn (0);
+{	puts("\"Programming is like building a multi lingual puzzle");
+	Return(0);
 }
