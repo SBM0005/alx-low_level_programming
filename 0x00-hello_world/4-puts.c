597e5071251ec446c69c4595da5db3a3ff69ae2d
Sbonokuhle <b.sbonokuhle@gmail.com>
2023-02-20 07:24:29 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: Print a line of code using puts
+ * main - Print a line of code using puts
  * return: Always 0
  */
 int main(void)
